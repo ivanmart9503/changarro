68f37418c41620b6f77766237bf9786c23e1ad59
Iván Martínez <ivanmart9503@gmail.com>
2019-06-16 10:46:36 -0500

    Se eliminó la carpeta de temas

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:changarro/src/home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import './src/helpers/themes.dart';
 
 void main() => runApp(Changarro());
 
